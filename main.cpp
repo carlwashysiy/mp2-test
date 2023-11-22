e9d7e85b6c92218b2aced3776dbfa30a512649f3
Carl PC <siycarlwashington@gmail.com>
2023-11-22 18:05:41 +0800

    third

--- main.cpp
+++ main.cpp
@@ -4,15 +4,5 @@ using namespace std;
 int main()
 {
     cout <<"Hello world"<<endl;
-    cout <<"henzer"<<endl;
-    cout <<"balssss"<<endl;
-    cout <<"yoyo"<<endl;
-    cout <<"yoyomaaaaa"<<endl;
-    cout <<"hey bro"<<endl;
-    cout <<"Hello world"<<endl;
-    cout <<"bvru"<<endl;
-    cout <<"hey bro"<<endl;
-    cout <<"Hello world"<<endl;
-    cout <<"bvru"<<endl;
     cout <<"hey bro"<<endl;
 }
