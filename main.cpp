7da9a914118a211556ec3db40f2e4ec048bb87ea
Carl <carl.siy@obf.ateneo.edu>
2023-11-22 17:57:34 +0800

    shiftalt a

--- main.cpp
+++ main.cpp
@@ -6,4 +6,10 @@ int main()
     cout <<"Hello world"<<endl;
     cout <<"bvru"<<endl;
     cout <<"hey bro"<<endl;
+    cout <<"Hello world"<<endl;
+    cout <<"bvru"<<endl;
+    cout <<"hey bro"<<endl;
+    cout <<"Hello world"<<endl;
+    cout <<"bvru"<<endl;
+    cout <<"hey bro"<<endl;
 }
