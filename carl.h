93db0f00d310a7a2fbc66805d33cb46ac89e75e4
Carl <carl.siy@obf.ateneo.edu>
2023-11-24 13:50:08 +0800

    added header

---
+++ carl.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void add(int x,int y,int&z)
+{
+    z= x+y;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "carl.h"
 using namespace std;
 
 int main()
 {
-    cout <<"Hello world"<<endl;
+    cout <<"input 2 numbers"<<endl;
+    cout <<"number 1: ";
+    int x;
+    cin >>x;
+    cout <<"number 2: ";
+    int y;
+    cin >>y;
+    int z;
+    cout <<"adding numbers...\n";
+    add(x,y,z);
+    cout <<"sum is: "<<z;
     
-    cout <<"niceee"<<endl;
 }
